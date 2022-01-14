bd70f229c386167195593f6152499f6bdcf9fd7d
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-01-13 22:35:10 -0300

    Criaçao do projeto e Componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { LivrariaAppComponent } from './components/livraria-app/livraria-app.component';
+import { FiltersComponent } from './components/livraria-app/filters/filters.component';
+import { ProductListComponent } from './components/livraria-app/product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    LivrariaAppComponent,
+    FiltersComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/livraria-app/livraria-app.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LivrariaAppComponent } from './livraria-app.component';
+
+describe('LivrariaAppComponent', () => {
+  let component: LivrariaAppComponent;
+  let fixture: ComponentFixture<LivrariaAppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LivrariaAppComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LivrariaAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
